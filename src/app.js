5c8d695388822d421d8738f1aad61ab934ae22f0
slndrbbe <125832173+rdvlpr@users.noreply.github.com>
2023-07-07 18:54:02 +0100

    Created function to load London weather and search city

--- src/app.js
+++ src/app.js
@@ -55,3 +55,50 @@ function formatDay(timestamp) {
 
 let date = document.querySelector("#date");
 date.innerHTML = updateDayTime(currentDate);
+
+// DISPLAY TEMPERATURE //
+
+function displayTemperature(response) {
+  let cityElement = document.getElementById("city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let feelsLikeElement = document.getElementById("feels-like");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+
+  let celsiusTemperature = response.data.daily[0].temperature.day;
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  descriptionElement.innerHTML = response.data.daily[0].condition.description;
+  feelsLikeElement.innerHTML = Math.round(
+    response.data.daily[0].temperature.day
+  );
+  humidityElement.innerHTML = response.data.daily[0].temperature.humidity;
+  windElement.innerHTML = Math.round(
+    response.data.daily[0].wind.speed * 2.23694
+  );
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[0].condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.daily[0].condition.icon);
+}
+
+function search(city) {
+  let apiKey = "7b7b503c248bt33a9a6fcobbea38af51";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("London");
