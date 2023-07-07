e70786b0c006ce6973aef0739b3d489f07ebc698
slndrbbe <125832173+rdvlpr@users.noreply.github.com>
2023-07-07 20:43:09 +0100

    Concatenated HTML and created functions to format and display forecast

--- src/app.js
+++ src/app.js
@@ -56,7 +56,61 @@ function formatDay(timestamp) {
 let date = document.querySelector("#date");
 date.innerHTML = updateDayTime(currentDate);
 
+// DISPLAY FORECAST //
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
+    <div class="col-2">
+      <div class="weather-forecast-date">
+      ${formatDay(forecastDay.time)}
+      </div>
+        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+          forecastDay.condition.icon
+        }.png" class="forecast-icon">
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max"> ${Math.round(
+          forecastDay.temperature.maximum
+        )}°</span>
+        <span class="weather-forecast-temperature-min"> ${Math.round(
+          forecastDay.temperature.minimum
+        )}°</span>
+      </div>
+      <div 
+      class="weather-forecast-desc">
+      Sunny 
+      </div>
+      </div>
+   `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(city) {
+  let apiKey = "7b7b503c248bt33a9a6fcobbea38af51";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 // DISPLAY TEMPERATURE //
+
+function getForecast(city) {
+  let apiKey = "7b7b503c248bt33a9a6fcobbea38af51";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let cityElement = document.getElementById("city");
@@ -65,7 +119,7 @@ function displayTemperature(response) {
   let feelsLikeElement = document.getElementById("feels-like");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let iconElement = document.querySelector("#icon");
+  let iconElement = document.querySelector("#main-icon");
 
   let celsiusTemperature = response.data.daily[0].temperature.day;
 
@@ -84,6 +138,8 @@ function displayTemperature(response) {
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[0].condition.icon}.png`
   );
   iconElement.setAttribute("alt", response.data.daily[0].condition.icon);
+
+  getForecast(response.data.city);
 }
 
 function search(city) {
